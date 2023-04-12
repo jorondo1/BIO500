82e43796ee395f7e8de9873f6279fa58cd20297a
Jonathan Rondeau-Leclaire <jorondo@MacBook-Pro-von-Jonathan.local>
2023-04-12 13:11:15 -0400

    init

---
+++ scripts/figures.R
@@ -0,0 +1,4 @@
+library(pacman)
+p_load(tidygraph, igraph, dplyr, #GGally, sna, intergraph, sparsebnUtils, spaa, 
+       network, magrittr, RColorBrewer, NetworkToolbox, tibble, rstatix, ggpubr,
+       visNetwork)
